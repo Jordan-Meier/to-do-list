28083abfab0d476780508390eaa02e4a4eb68339
Anna Becker and Jordan Meier <jbirdmeier@gmail.com>
2016-01-25 12:01:06 -0800

    Assign unique id to a new task that user creates

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,24 @@ function Task(name) {
 }
 
 $(document).ready(function() {
+  $("form#new-task").submit(function(event) {
+
+    var inputName = $("input#new-task-name").val();
+    var newTask = new Task(inputName);
+
+    $("ul#tasks").append("<li id=" + newTask.name + "><span class= 'taskName'>" + newTask.name +  "</span></li>");
+
+    $("input#new-task-name").val("");
+
+    $(# + newTask.name).click(function() {
+      // $("#show-tasks").show();
+    });
+
+
+    event.preventDefault();
+
+
+  });
 
 
 });
